537e47efe22db6e6d66357d71ca04dd750b053c0
hg16 <hmgaar@gmail.com>
2016-01-22 17:12:40 -0500

    creating products seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 # categories = [
 #   {
 #   name: "Inverter Mini-splits",
@@ -25,157 +24,120 @@
 #   },
 #   {
 #   name: "Commercial air conditioners",
-#   description: "Packaged Units And Large Scale Equipment for Businesses, Hotels, and Industrial Needs."
+#   description: "Packaged Units And Large Scale Equipment for Businesses, Hotels, and Industrial Needs.",
+#   image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FS%5F%5FZFSeries%5F34%2Ejpg&width=0&height=150"
 #   },
 #   {
 #   name: "On/off mini-splits",
-#   description: "On/Off Mini-Splits Offer Great Value For The Cooling Of An Isolated Space."
+#   description: "On/Off Mini-Splits Offer Great Value For The Cooling Of An Isolated Space.",
+#   image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FMWFFT%281%29%2Ejpg&width=0&height=140"
 #   },
 #   {
 #   name: "Refrigeration",
-#   description: "Condensing Units and Evaporators for Reach-In Coolers, Walk-In Coolers, and Freezers."
+#   description: "Condensing Units and Evaporators for Reach-In Coolers, Walk-In Coolers, and Freezers.",
+#   image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FEvaporador%5FBohn%2Ejpg&width=200&height=0"
 #   },
 #   {
 #   name: "Room Air Conditioners, Portables and Dehumidifiers",
-#   description: "Ductless, Pipeless Air Conditioning Units Housed All Together, Typically To Cool One Room."
+#   description: "Ductless, Pipeless Air Conditioning Units Housed All Together, Typically To Cool One Room.",
+#   image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FMPNT2%2Ejpg&width=200&height=0"
 #   },
 #   {
 #   name:"Fan Coils, Universal Mounts, and Cassettes",
-#   description:"Fan Coils For A Wide Range Of Applications."
+#   description:"Fan Coils For A Wide Range Of Applications.",
+#   image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FFan%5FCoil2%2Ejpg&width=200&height=0"
 #   },
 #   {
 #   name: "Compressors",
-#   description:"Ample Variety In Order to Meet All Residential, Commercial, and Industrial Needs."
+#   description:"Ample Variety In Order to Meet All Residential, Commercial, and Industrial Needs.",
+#   image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FCopeland%5FScroll%5FCompressor3%2Ejpg&width=0&height=150"
 #   },
 #   {
 #   name:"Controls",
-#   description:"Control, Monitoring and Automation Products for Cooling Systems."
+#   description:"Control, Monitoring and Automation Products for Cooling Systems.",
+#   image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FPRO1%5FIAQ2%2Ejpg&width=0&height=150"
 #   },
 #   {
 #   name:"Insulation",
-#   description:"Solutions for Making Ducts in Air Conditioning, Heating, and Ventilation."
+#   description:"Solutions for Making Ducts in Air Conditioning, Heating, and Ventilation.",
+#   image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FPlancha%5FPoliuretano%281%29%2Ejpg&width=0&height=150"
 #   }
-# ## NOTE TO BE CONTINUED
 # ]
-# category = Category.create(categories)
+# category = Category.create(categories) TODO THIS IS ALREADY IN PRODUCTION NO NEED TO SEED IT.
 
-
-categories = [
+products = [
   {
-  name: "Inverter Mini-splits",
-  description: "Wide Range of Product Offerings Including High Efficiency and Inverter Units",
-  image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FSplit%5FPared%5FUltra%5FPremier%2Ejpg&width=0&height=150"
+    name:"",
+    # price:0,
+    description:"",
+    image: "",
+    logo:"",
+    category:"",
+    mod_name:""
   },
   {
-  name: "Ducted systems",
-  description:"Single-phase Split Systems with a capacity from 1-5 Tons",
-  image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FWestinghouse%5FAC3%2Ejpg&width=0&height=140"
+    name:"",
+    # price:0,
+    description:"",
+    image: "",
+    logo:"",
+    category:"",
+    mod_name:""
   },
   {
-  name:"LG duty free systems",
-  description: "LG's Duct-Less Single Zone And Multi-Zone Offerings Reduce Contractor Costs And Save Consumers Money On Electricity",
-  image: "http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FMulti%5FF%2Ejpg&width=0&height=140"
+    name:"",
+    # price:0,
+    description:"",
+    image: "",
+    logo:"",
+    category:"",
+    mod_name:""
   },
   {
-  name: "Commercial air conditioners",
-  description: "Packaged Units And Large Scale Equipment for Businesses, Hotels, and Industrial Needs.",
-  image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FS%5F%5FZFSeries%5F34%2Ejpg&width=0&height=150"
+    name:"",
+    # price:0,
+    description:"",
+    image: "",
+    logo:"",
+    category:"",
+    mod_name:""
   },
   {
-  name: "On/off mini-splits",
-  description: "On/Off Mini-Splits Offer Great Value For The Cooling Of An Isolated Space.",
-  image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FMWFFT%281%29%2Ejpg&width=0&height=140"
+    name:"",
+    # price:0,
+    description:"",
+    image: "",
+    logo:"",
+    category:"",
+    mod_name:""
   },
   {
-  name: "Refrigeration",
-  description: "Condensing Units and Evaporators for Reach-In Coolers, Walk-In Coolers, and Freezers.",
-  image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FEvaporador%5FBohn%2Ejpg&width=200&height=0"
+    name:"",
+    # price:0,
+    description:"",
+    image: "",
+    logo:"",
+    category:"",
+    mod_name:""
   },
   {
-  name: "Room Air Conditioners, Portables and Dehumidifiers",
-  description: "Ductless, Pipeless Air Conditioning Units Housed All Together, Typically To Cool One Room.",
-  image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FMPNT2%2Ejpg&width=200&height=0"
+    name:"",
+    # price:0,
+    description:"",
+    image: "",
+    logo:"",
+    category:"",
+    mod_name:""
   },
   {
-  name:"Fan Coils, Universal Mounts, and Cassettes",
-  description:"Fan Coils For A Wide Range Of Applications.",
-  image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FFan%5FCoil2%2Ejpg&width=200&height=0"
-  },
-  {
-  name: "Compressors",
-  description:"Ample Variety In Order to Meet All Residential, Commercial, and Industrial Needs.",
-  image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FCopeland%5FScroll%5FCompressor3%2Ejpg&width=0&height=150"
-  },
-  {
-  name:"Controls",
-  description:"Control, Monitoring and Automation Products for Cooling Systems.",
-  image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FPRO1%5FIAQ2%2Ejpg&width=0&height=150"
-  },
-  {
-  name:"Insulation",
-  description:"Solutions for Making Ducts in Air Conditioning, Heating, and Ventilation.",
-  image:"http://www.refripartesrd.com/inc/ewbv61.asp?fn=%2Fuploads%2FPlancha%5FPoliuretano%281%29%2Ejpg&width=0&height=150"
+    name:"",
+    # price:0,
+    description:"",
+    image: "",
+    logo:"",
+    category:"",
+    mod_name:""
   }
 ]
-category = Category.create(categories)
-
-# products = [
-#   {
-#     name:"",
-#     price:0,
-#     description:"",
-#     image: "",
-#     category:""
-#   },
-#   {
-#     name:"",
-#     price:0,
-#     description:"",
-#     image: "",
-#     category:""
-#   },
-#   {
-#     name:"",
-#     price:0,
-#     description:"",
-#     image: "",
-#     category:""
-#   },
-#   {
-#     name:"",
-#     price:0,
-#     description:"",
-#     image: "",
-#     category:""
-#   },
-#   {
-#     name:"",
-#     price:0,
-#     description:"",
-#     image: "",
-#     category:""
-#   },
-#   {
-#     name:"",
-#     price:0,
-#     description:"",
-#     image: "",
-#     category:""
-#   },
-#   {
-#     name:"",
-#     price:0,
-#     description:"",
-#     image: "",
-#     category:""
-#   },
-#   {
-#     name:"",
-#     price:0,
-#     description:"",
-#     image: "",
-#     category:""
-#   }
-# ]
 #
-# product = Product.create(products)
+product = Product.create(products)
