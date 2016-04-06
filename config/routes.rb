ed52d19e5d35be6538da5303aff79c72507b6ed2
pkellz6 <pkellz6@me.com>
2016-04-06 19:44:28 -0400

    homepage frontend edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'brands', to: "categories#brands"
+  get '/about' => "pages#about"
   resources :categories#, param: :id do
   resources :products #, param: :id do TODO HOW TO RELATE THEM
       # end
