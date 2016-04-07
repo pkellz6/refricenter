30d13250b1667324919c8a8ea1921085f79039e5
pkellz6 <pkellz6@me.com>
2016-04-07 14:49:47 -0400

    contact page, social media, and brand logos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'brands', to: "categories#brands"
   get '/about' => "pages#about"
+  get '/contact' => "pages#contact"
   resources :categories#, param: :id do
   resources :products #, param: :id do TODO HOW TO RELATE THEM
       # end
