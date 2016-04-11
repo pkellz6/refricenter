e9ae8f87570c74561c9e587e773dd5badf7f4aa2
pkellz6 <pkellz6@me.com>
2016-04-11 17:35:27 -0400

    Added locations page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'brands', to: "categories#brands"
   get '/about' => "pages#about"
   get '/contact' => "pages#contact"
+  get '/locations' => "pages#locations"
+
   resources :categories#, param: :id do
   resources :products #, param: :id do TODO HOW TO RELATE THEM
       # end
